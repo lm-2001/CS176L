e6c891ca6c14e92f87f5767358eb47fa2ab6ce7e
lm-2001 <55418637+lm-2001@users.noreply.github.com>
2022-04-17 14:28:13 -0400

    Add files via upload

---
+++ Quiz.java
@@ -0,0 +1,10 @@
+public class Quiz implements Measurable {
+	
+   private double score;
+   public Quiz(double score) {
+      this.score = score;
+   }
+   public double getMeasure(){
+      return score;
+   }
+}
